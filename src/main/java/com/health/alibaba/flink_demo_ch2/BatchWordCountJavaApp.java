b038fa922f49307158d1e5b048e9ee1f0fcabc32
wjy <316057621@qq.com>
2020-03-28 15:01:19 +0800

    flink-wordcount

---
+++ src/main/java/com/health/alibaba/flink_demo_ch2/BatchWordCountJavaApp.java
@@ -0,0 +1,45 @@
+package com.health.alibaba.flink_demo_ch2;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ *  Java API 开发flink的批处理应用程序
+ * @Author: wjy
+ * @Date: 2020/3/27 16:12
+ */
+public class BatchWordCountJavaApp {
+    public static void main(String[] args) throws Exception {
+
+        String input = "C:\\Users\\MrWang\\Desktop\\new 1.txt";
+
+        // 1. 获取执行环境环境的上下文
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 2.读取数据
+        DataSource<String> myText = env.readTextFile(input);
+
+        // 3.业务逻辑处理
+        // 每一行按照指定分隔符进行拆分，使用map来进行统计
+        // 按照第一个tuple中的第一个字符进行group，然后按照第二个字符进行sum求和
+        myText.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                // 将字符串拆分，并为每一个附上一个1
+                String[] words = s.split(" ");
+                for (String word : words) {
+                    collector.collect(new Tuple2<>(word, 1));
+                }
+            }
+        })
+                .groupBy(0)
+                .sum(1)
+                .print();
+
+    }
+}
---
+++ src/main/java/com/health/alibaba/flink_demo_ch2/StreamWordCountJavaApp.java
@@ -0,0 +1,53 @@
+package com.health.alibaba.flink_demo_ch2;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * 使用java api 处理实时应用程序word count
+ * 所以wordcount的数据会从一个socket来获得，所以是源源不断的
+ * @Author: wjy
+ * @Date: 2020/3/27 23:53
+ */
+public class StreamWordCountJavaApp {
+    public static void main(String[] args) {
+
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String canshu = parameterTool.get("canshu");
+        Integer port = parameterTool.getInt("port");
+        // step1 获取流处理环境
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        // step2 读取流文件（例如socke文件）
+        DataStreamSource<String> streamText = executionEnvironment.socketTextStream("localhost", 9999);
+        // step3 对流文件进行业务操作
+        streamText.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] words = value.split(" ");
+                for(String word : words) {
+                    out.collect(new Tuple2<>(word, 1));
+                }
+            }
+        }).filter((FilterFunction<Tuple2<String, Integer>>) value -> {
+            if(value == null) {
+                return false;
+            }
+            return true;
+        }).keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1)
+                .print()
+                .setParallelism(1);
+        try {
+            executionEnvironment.execute("first-stream-ontime-job");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
